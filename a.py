66ae66cff72c459d305a82b7bbe0d7b07b853f93
ammaar21 <144823695+ammaar21@users.noreply.github.com>
2023-09-12 16:26:08 +0100

    Create a.py

---
+++ a.py
@@ -0,0 +1,14 @@
+select = int(input("chose number from 1, 2, 3, 4:"))
+a = int(input("enter number"))
+b = int(input("enter number"))
+if select == 1:
+    print(a + b)
+elif select == 2:
+    print(a - b)
+elif select == 3:
+    print(a * b)
+elif select == 3:
+    print(a / b)
+else:
+    print("nope")
+
